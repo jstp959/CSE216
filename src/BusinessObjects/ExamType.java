165bd26c493b0d7f25a1fc07e66034fec9673aa9
Rachel Okun <ryo218@lehigh.edu>
2016-11-10 20:27:22 -0500

    ExamType added
    wrote in eclipse …

--- src/BusinessObjects/ExamType.java
+++ src/BusinessObjects/ExamType.java
@@ -6,9 +6,72 @@
 package BusinessObjects;
 
 /**
+ * Blah
  *
- * @author elisecross
+ * @author Rachel
+ * @version Nov 10, 2016
  */
-public class ExamType {
-    
+public class ExamType implements Comparable<ExamType> {
+
+    private String name;
+    private String description;
+    private Boolean status;
+
+    // ----------------------------------------------------------
+    /**
+     * Create a new Exam object.
+     *
+     * @param name exam type!!
+     * @param description explanation of exam type
+     */
+    public ExamType(String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.status = true; // active
+    }
+
+    // ----------------------------------------------------------
+    /**
+     * Get the name of an Exam Type
+     *
+     * @return the name of the Exam Type
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    // ----------------------------------------------------------
+    /**
+     * Get an Exam Type's description
+     *
+     * @return the description associated with the Exam Type, if applicable
+     */
+    public String getDescription() {
+        return this.description;
+    }
+
+    // ----------------------------------------------------------
+    /**
+     * Get an Exam Type's status (active or inactive)
+     *
+     * @return the status of the Exam Type (1 = active; 0 = inactive)
+     */
+    public Boolean getStatus() {
+        return this.status;
+    }
+
+    /**
+     * Compares exams.
+     *
+     * @param examtype another exam
+     * @return a number (negative or zero)
+     */
+    public int compareTo(ExamType examtype) {
+        if (!this.name.equals(examtype.name)) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
 }
